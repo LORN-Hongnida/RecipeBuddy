598ad901c35dba9114b99940bb44a5eebb59bdda
LORN-Hongnida <137864918+LORN-Hongnida@users.noreply.github.com>
2025-04-23 00:30:21 +0700

    connect to firebase and spoonacular

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,22 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Import the generated firebase_options.dart file
 import 'pages/splash_screen.dart';
 
-
-void main() =>runApp(
-  DevicePreview(
-    enabled: !kReleaseMode,
-    builder: (context) => MyApp(),
-  )
-);
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => MyApp(),
+    ),
+  );
+}
 
 class MyApp extends StatelessWidget {
   @override
